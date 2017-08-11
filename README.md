c5bbc281dba3a1e07338977fdca3631c2068c021
LD MacKrell <ldmackrell@gmail.com>
2017-08-11 11:12:54 -0700

    add branching functionality to document using JS.

---
+++ README.md
@@ -0,0 +1,52 @@
+# _Coding Languages Quiz_
+
+#### _A short quiz designed to match the user with suggested coding language to learn, 08.11.17_
+
+#### By _**L. Devin MacKrell**_
+
+## Description
+
+_This is a quiz that is designed to match up the user's interest and career goals with suggested programming language based on their answers to a series of questions._
+
+## Setup/Installation Requirements
+
+* _Clone this repository_
+* _Open simple HTTP server in the top level of the cloned directory._
+* _Then open the application in your browser of choice._
+
+## Known Bugs
+
+_No bugs or issues have been identified in this application. If detected, please contact the developer at ldmackrell@gmail.com._
+
+## Support and contact details
+
+_If you run into any issues or have questions/ideas/concerns, please contact ldmackrell@gmail.com.  Code contributions/improvements are welcome and can be submitted to the same contact._
+
+## Technologies Used
+
+_Basic web development tools including:
+JavaScript
+jQuery
+Bootstrap_
+
+### License
+
+Copyright (c) 2017 _L. Devin MacKrell_
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
---
+++ js/scripts.js
@@ -0,0 +1,102 @@
+$(document).ready(function(){
+  var front, back, yes, no, big, small, pro, con
+  $(".page1btn").click(function(){
+    $(".page").addClass("hide");
+    $(".page1").removeClass("hide");
+  });
+  
+  $(".page2btn").click(function(){
+    if ($("input[value='front']").prop("checked")){
+      front = true;
+      $(".page").addClass("hide");
+      alert('CSS_Design');
+      //finalPage();
+    }
+    else if ($("input[value='back']").prop("checked")){
+      back = true;
+      $(".page").addClass("hide");
+      $(".page2").removeClass("hide");
+    }
+    else{
+      alert ('Please select an option!');
+    }
+  });
+
+  $(".page3btn").click(function(){
+    if ($("input[value='yes']").prop("checked")){
+      yes = true;
+      $(".page").addClass("hide");
+      $(".page4").removeClass("hide");
+    }
+    else if ($("input[value='no']").prop("checked")){
+      no = true;
+      $(".page").addClass("hide");
+      $(".page4").removeClass("hide");
+    }
+    else{
+      alert ('Please select an option');
+    }
+  });
+
+  $(".page4btn").click(function(){
+    if ($("input[value='big']").prop("checked")){
+      big = true;
+      $(".page").addClass("hide");
+      if (back == true && no == true){
+        alert('Java');
+        //finalPage();
+      }
+      else if (yes == true){
+        $(".page5").removeClass("hide");
+      }
+      else{
+        alert('Please select an option!')
+      }
+    }
+    else if ($("input[value='small']").prop("checkout")){
+      small = true;
+      $(".page").addClass("hide");
+      if (back == true && no == true){
+        alert('Ruby');
+        //finalPage();
+      }
+      else {
+        alert ('Please select an option!')
+      }
+    }
+    else{
+      alert ('Please select an option!')
+    }
+
+  });
+
+
+  $(".page5btn").click(function(){
+    if ($("input[value='pro']").prop("checkout")){
+      pro = true;
+      $(".page").addClass("hide");
+      if(back == true && yes == true && big == true){
+        alert('C#');
+        //finalPage();
+      }
+      else{
+        alert ('Please select an option')
+      }
+    }
+    else if ($("input[value='con']").prop("checkout")){
+      con = true;
+      $(".page1").removeClass("hide");
+      if(back = true && yes == true && big == true){
+        alert('PHP');
+        //finalPage();
+      }
+      else{
+        alert ('Please select an option!')
+      }
+    }
+    else{
+      alert ('Please select an option!')
+    }
+  });
+
+});
